1ef828e05eb6153bc7d26f215f0c16bfcfbb24fd
RostikRK <91615985+RostikRK@users.noreply.github.com>
2022-02-11 19:24:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ import re
 import csv
 from haversine import *
 from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
 from operator import itemgetter
+from geopy.exc import GeocoderUnavailable
 
 def delete_multiple_element(list_object, indices):
+    """
+    Deletes from list multiple indexes which are in list
+    >>> delete_multiple_element([51, 52, 53, 54, 55, 56, 57, 58, 59], [4, 2, 6])
+    [51, 52, 54, 56, 58, 59]
+    """
     indices = sorted(indices, reverse=True)
     for idx in indices:
         if idx < len(list_object):
             list_object.pop(idx)
 
-def convert_list_to_csv(path_to_data_set: str, year: int, user_latitude, user_longitude) -> list:
+def convert_list_to_csv(path_to_data_set: str, year: int, user_latitude, user_longitude):
     """
     Converts the file with format list into a scv of ten the nearest to user points
     """
@@ -47,14 +54,18 @@ def convert_list_to_csv(path_to_data_set: str, year: int, user_latitude, user_lo
             bad_year_indexes.append(normal_list.index(yearr))
     delete_multiple_element(normal_list, bad_year_indexes)
     for film in normal_list:
-        geolocator = Nominatim(user_agent="GeoFinder")
-        location = geolocator.geocode(film[-1])
-        if location == None:
+        try:
+            geolocator = Nominatim(user_agent="GeoFinder")
+            geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.5)
+            location = geolocator.geocode(film[-1])
+            if location == None:
+                del film[-1]
+            else:
+                tupple = (location.latitude, location.longitude)
+                film.append(tupple[0])
+                film.append(tupple[1])
+        except GeocoderUnavailable:
             del film[-1]
-        else:
-            tupple = (location.latitude, location.longitude)
-            film.append(tupple[0])
-            film.append(tupple[1])
     bad_len_indexes = []
     for ellem in normal_list:
         if len(ellem) != 5:
@@ -72,6 +83,10 @@ def convert_list_to_csv(path_to_data_set: str, year: int, user_latitude, user_lo
         writer = csv.writer(f)
         writer.writerow(header)
         writer.writerows(normal_list[0:10])
+    with open('all_films.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(normal_list)
 
 
 def main():
@@ -92,11 +107,19 @@ def main():
     latitudee = data["latitude"]
     longiitude = data["longitude"]
     film_name = data['name']
+    dataal = pandas.read_csv("all_films.csv")
+    latitudee_all = dataal["latitude"]
+    longiitude_all = dataal["longitude"]
+    film_name_all = dataal['name']
     mapy = folium.Map(location=[args.user_latitude, args.user_longitude], zoom_start=10)
     fg = folium.FeatureGroup(name="Films_map")
     for latit, longi, namme in zip(latitudee, longiitude, film_name):
         fg.add_child(folium.Marker(location=(latit, longi), radius=10, popup=str(args.year) + "\n" + namme,\
 icon=folium.Icon(color="red", icon="info-sign")))
+    fg_al = folium.FeatureGroup(name="Films_map")
+    for latit, longi, namme in zip(latitudee_all, longiitude_all, film_name_all):
+        fg_al.add_child(folium.Marker(location=(latit, longi), radius=10, popup=str(args.year) + "\n" + namme, \
+                                   icon=folium.Icon(color="blue", icon="info-sign")))
     fg_pp = folium.FeatureGroup(name="Population")
     fg_pp.add_child(folium.GeoJson(data=open('world.json', 'r',
                                              encoding='utf-8-sig').read(),
@@ -105,8 +128,10 @@ icon=folium.Icon(color="red", icon="info-sign")))
                                                                  else 'orange' if 10000000 <= x['properties'][
                                                                      'POP2005'] < 20000000
                                                                  else 'yellow'}))
+    mapy.add_child(fg_al)
     mapy.add_child(fg_pp)
     mapy.add_child(fg)
+    mapy.add_child(folium.LayerControl())
     mapy.save('Great_map.html')
 
 
